83856a35b7c1db96ffd3b197da3e23249dd8b5c9
Yusuke Kawamoto <novogrammer@gmail.com>
2025-06-02 17:15:29 +0900

    同じURLでも通常のGETとSSEを切り替えるようにする。

--- next-app/src/app/api/messages/[id]/route.ts
+++ next-app/src/app/api/messages/[id]/route.ts
@@ -1,12 +1,23 @@
 import { sendEventStream } from "../../../../libs/event_stream_utils";
 import { makeErrorResponse } from "../../../../libs/api_utils";
+import { NextResponse } from "next/server";
 
 
 export async function GET(request:Request,{params}: { params: Promise<{ id: string }>}){
+  const accept = request.headers.get("accept") || "";
   try{
     const {id}=await params;
     
     const text = `${id} 夜空に広がる無数の星々の中、ひときわ明るく輝く星がありました。`
+    if(!accept.includes("text/event-stream")){
+
+      return NextResponse.json({
+        // event: "",
+        data: text
+      },{
+        status:200,
+      });
+    }
 
     let index = 0;
     return sendEventStream(async ()=>{
@@ -15,6 +26,7 @@ export async function GET(request:Request,{params}: { params: Promise<{ id: stri
         const data=text[index];
         index++;
         return {
+          // event: "",
           data,
           isDone:false,
         }
--- next-app/src/app/api/sse/route.ts
+++ next-app/src/app/api/sse/route.ts
@@ -1,9 +1,21 @@
 import { sendEventStream } from "../../../libs/event_stream_utils";
 import { makeErrorResponse } from "../../../libs/api_utils";
+import { NextResponse } from "next/server";
 
-export async function GET(/* request:Request */){
+export async function GET(request:Request){
+  
+  const accept = request.headers.get("accept") || "";
+
   try{
-    const text = '夜空に広がる無数の星々の中、ひときわ明るく輝く星がありました。'
+    const text = '夜空に広がる無数の星々の中、ひときわ明るく輝く星がありました。';
+    if(!accept.includes("text/event-stream")){
+
+      return NextResponse.json({
+        data: text
+      },{
+        status:200,
+      });
+    }
 
     let index = 0;
     return sendEventStream(async ()=>{
